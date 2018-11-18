f89ded745a1ff5e4f4f4eb6505e26f5faf2db4ad
vuvanlinh <vuvanlinh95@gmail.com>
2018-11-18 22:23:38 +0700

    check leap year

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+
+        boolean isLeapYear = false;
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
+}
